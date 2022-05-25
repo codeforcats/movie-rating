34f2c1afeabce5eabaac0807aa3445d5e4e33a24
codeforcats <richard.spurr@gmail.com>
2022-05-25 13:38:23 +0100

    added test for hard coded result

--- src/test/java/com/rjs/ratingsdataservice/RatingsDataServiceApplicationTests.java
+++ src/test/java/com/rjs/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.rjs.ratingsdataservice;
 
+import com.rjs.ratingsdataservice.model.MovieRating;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RatingsDataServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldReturnHardCodedMovieInfo(){
+		MovieRating movieRating = restTemplate.getForObject("http://localhost:" + port + "/movieRatings/1",
+				MovieRating.class);
+
+		assertThat(movieRating).isEqualTo(new MovieRating(1,4));
+	}
 }
